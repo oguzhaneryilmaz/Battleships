a09f441d4069214961745d09f675db361957128a
oguzhaneryilmaz <56223070+oguzhaneryilmaz@users.noreply.github.com>
2022-11-25 18:17:02 +0300

    ++
    1. oyuncu eşleşmesi yapıldı
    2. birbirlerinin haritasını görüyorlar vuruyorlar
    3. sırayla vurma mekaniği üzerine çalışılıyor

--- lib/wait_page.dart
+++ lib/wait_page.dart
@@ -1,11 +1,15 @@
 import 'package:battleships/firebase_database_functions.dart';
 import 'package:battleships/global_enough_player.dart';
+import 'package:battleships/global_is_hit_ship.dart';
 import 'package:battleships/view/gameStarted.dart';
 import 'package:battleships/view/homepage.dart';
 import 'package:battleships/viewmodel/main_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'enough_player.dart';
+import 'is_ship_hit.dart';
+import 'package:battleships/viewmodel/defineLists.dart';
+import 'firebase_database_functions.dart';
 
 class waitPage extends StatefulWidget {
   const waitPage({super.key});
@@ -16,6 +20,7 @@ class waitPage extends StatefulWidget {
 
 class _waitPageState extends State<waitPage> {
   final enoughPlayer = EnoughPlayer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +49,9 @@ class _waitPageState extends State<waitPage> {
               if (globalEnoughPlayer.playerEnough)
                 TextButton(
                   onPressed: () {
+                    if (player == whoseTurn) {
+                      isHitShip.didShipHit = true;
+                    }
                     playerMatch();
                     Navigator.push(
                         context,
